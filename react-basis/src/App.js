343d9a2303e241bfb5fac37d7e7a1ca781109df1
Angel Martinez <angel.martinez@wizeline.com>
2019-12-27 20:33:55 -0600

    Wip Section 5

--- react-basis/src/App.js
+++ react-basis/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component, useState } from 'react';
+import Radium, { StyleRoot } from 'radium';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
 import Person from './components/Person';
@@ -16,7 +18,7 @@ import Char from './components/Char';
 //       { name: 'Karla', age: 23 },
 //     ],
 //   });
-  
+
 //   const switchNameHandler = () => {
 //     setPersonsState({
 //       persons: [
@@ -28,22 +30,22 @@ import Char from './components/Char';
 //   }
 
 //   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
+//     <div className='App'>
+//       <header className='App-header'>
+//         <img src={logo} className='App-logo' alt='logo' />
+//           <h1 className='App-title'>Welcome to React</h1>
 //       </header>
 
-//       <p className="App-intro">
+//       <p className='App-intro'>
 //         To get started, edit <code>src/App.js</code> and save to reload.
 //       </p>
-        
+
 //       <h1> This is a really cool React App</h1>
 
 //       <hr/>
-        
+
 //       <button onClick={switchNameHandler}>Switch name</button>
-        
+
 //       < Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
 //     </div>
 //   );
@@ -52,113 +54,130 @@ import Char from './components/Char';
 // export default app;
 
 // Example with state to manipulate state
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'salmon' : 'purple'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lightgreen;
+    color: black;
+  },
+`;
 class App extends Component {
- state = {
-   persons: [
-     { id: 1, name: 'Lio', age: 28 },
-     { id: 2, name: 'Angel', age: 27 },
-     { id: 3, name: 'Karla', age: 23 },
-   ],
-   droids:[
-     { name: 'R2D2' },
-     { name: 'BB8' },
-     { name: 'D0' },
-   ],
-   toggle: true,
-   userInput: '',
- }
- 
-  droidsNameHandler = (event) => {
+  state = {
+    persons: [
+      { id: 1, name: 'Lio', age: 28 },
+      { id: 2, name: 'Angel', age: 27 },
+      { id: 3, name: 'Karla', age: 23 }
+    ],
+    droids: [{ name: 'R2D2' }, { name: 'BB8' }, { name: 'D0' }],
+    toggle: true,
+    userInput: ''
+  };
+
+  droidsNameHandler = event => {
     const { value } = event.target;
     this.setState({
-      droids:[
+      droids: [
         { name: value || 'DNLD' },
         { name: value || 'U9C4' },
-        { name: value || 'BC44' },
-      ],
-    })
-  }
+        { name: value || 'BC44' }
+      ]
+    });
+  };
 
-  switchNameHandler = (newName) => {
+  switchNameHandler = newName => {
     this.setState({
       persons: [
         { id: 1, name: newName, age: 28 },
         { id: 2, name: 'Miguel', age: 27 },
-        { id: 3, name: 'Lucero', age: 23 },
-      ],
+        { id: 3, name: 'Lucero', age: 23 }
+      ]
     });
-  }
+  };
 
   nameChangeHandler = (event, index) => {
     const personsInState = this.state.persons;
 
-    const personIndex = personsInState.findIndex(p => p.id  === index);
-    
+    const personIndex = personsInState.findIndex(p => p.id === index);
+
     const person = { ...personsInState[personIndex] };
 
     person.name = event.target.value;
-    
-    const persons = [ ...personsInState];
-    
+
+    const persons = [...personsInState];
+
     persons[personIndex] = person;
 
     this.setState({ persons });
-  }
+  };
 
-  inputChangeHandler = (event) => {
+  inputChangeHandler = event => {
     const userInput = event.target.value;
 
     this.setState({ userInput });
-  }
+  };
 
   togglePersonsHandler = () => {
     this.setState({
-      toggle: !this.state.toggle,
+      toggle: !this.state.toggle
     });
-  }
-  
-  deletePersonsHandler = (index) => {
-    const { persons } = this.state;
-    
-    persons.splice(index, 1);
-    
-    this.setState({ persons });
-  }
+  };
 
-  deleteCharHandler = (index) => {
+  deletePersonsHandler = index => {
+    const { persons } = this.state;
+
+    persons.splice(index, 1);
+
+    this.setState({ persons });
+  };
+
+  deleteCharHandler = index => {
     const chars = this.state.userInput.split('');
-    
+
     chars.splice(index, 1);
-    
+
     const updatedText = chars.join('');
-    
+
     this.setState({ userInput: updatedText });
-  }
-  
+  };
+
   render() {
     const style = {
-      backgroundColor: 'red',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer',     
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black',
+      },
     };
 
     let persons = null;
-    
-    const charList = this.state.userInput.split('').map((ch, index) =>
-      <Char
-        character={ch}
-        index={index}
-        key={index}
-        click={() => this.deleteCharHandler(index)}
-      />);
-    
+
+    const charList = this.state.userInput
+      .split('')
+      .map((ch, index) => (
+        <Char
+          character={ch}
+          index={index}
+          key={index}
+          click={() => this.deleteCharHandler(index)}
+        />
+      ));
+
     if (this.state.toggle) {
-      persons = 
-      <div>
-        {
-          this.state.persons.map((person, index) => <Person
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => (
+            <Person
               key={person.id}
               id={person.id}
               name={person.name}
@@ -166,64 +185,96 @@ class App extends Component {
               click={() => this.deletePersonsHandler(index)}
               // Binding with anonymous function
               // click={() => this.switchNameHandler('Leo')}
-              change={(event) => this.nameChangeHandler(event, person.id)}
+              change={event => this.nameChangeHandler(event, person.id)}
             />
-          )
-        }
-      </div>
+          ))}
+        </div>
+      );
+
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'cyan',
+        color: 'black',
+      };
+    }
+    
+    // let classes = ['red', 'bold'].join(' ');
+    let classes = [];
+    
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      // To use radium
+      // <StyleRoot>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
         </header>
         
+        <StyledButton
+          alt={this.state.toggle}
+          onClick={this.togglePersonsHandler}
+        >
+          Styled button
+        </StyledButton>
+
         <button
+          key='button1'
           style={style}
           // Binding with this
-          onClick={this.switchNameHandler.bind(this, 'Lionel')}>
+          onClick={this.switchNameHandler.bind(this, 'Lionel')}
+        >
           Switch name
         </button>
 
         <button
+          key='button2'
           style={style}
-          onClick={this.togglePersonsHandler}>
+          onClick={this.togglePersonsHandler}
+        >
           Show persons
         </button>
-        
+
         {persons}
-        
+
         <UserInput
           change={this.droidsNameHandler}
           currentName={this.state.droids[0].name}
         />
 
-        <UserOutput
-          name={this.state.droids[0].name}
-        />
+        <UserOutput name={this.state.droids[0].name} />
 
-        <UserOutput
-          name={this.state.droids[1].name}
-        />
+        <UserOutput name={this.state.droids[1].name} />
 
-        <UserOutput
-          name={this.state.droids[2].name}
+        <UserOutput name={this.state.droids[2].name} />
+
+        <input
+          type='text'
+          onChange={this.inputChangeHandler}
+          value={this.state.userInput}
         />
-        
-        <input type='text' onChange={this.inputChangeHandler} value={this.state.userInput} />
 
         <p>~{this.state.userInput}~</p>
 
-        <Validation inputLength={this.state.userInput.length}/>
-        
+        <Validation inputLength={this.state.userInput.length} />
+
         {charList}
       </div>
+      // </StyleRoot>
     );
     // Creating a rect element
     // return React.createElement('div', { className: 'App' }, 'This is my text');
   }
 }
 
+// To use radium
+// export default Radium(App);
 export default App;
--- react-basis/src/components/Person/index.js
+++ react-basis/src/components/Person/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import Radium from 'radium';
+import styled from 'styled-components';
+
 import './Person.css';
+
+const StyledDiv = styled.div`
+  color: red;
+  font-size: 18px;
+  padding: 20px;
+  width: 50%;
+  border: 2px solid red;
+`;
 
 const person = (props) => {
   const { name, age, id, click, change } = props;
+  
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px',
+    },
+  };
 
   return (
+  // To use radium
+  // <div className="Person" style={style}>
   <div className="Person">
     <p onClick={click}>ID: {id} - I'm {name} - {age} years old!</p>
     <p>{props.children}</p>
     <input type="text" onChange={change} value={name} />
+    <StyledDiv>I'm a div created with styled components</StyledDiv>
   </div>
   )
 }
 
+// To use radium
+// export default Radium(person);
 export default person;
